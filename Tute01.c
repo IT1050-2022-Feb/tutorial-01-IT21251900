e196d2d99ae1088fffc265f5d21d448cef0f9281
Rajapaksha R.M.S.D <100860580+IT21251900@users.noreply.github.com>
2022-03-03 16:22:52 +0000

    Tute01 Tute02 Tute03 Tute04

--- Tute01.c
+++ Tute01.c
@@ -1,11 +1,18 @@
 /* Exercise 1 - Calculations
-
    Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
 
 #include <stdio.h>
 
 int main() {
-  
+  int mark, i, sum = 0;
+  float avg;
+  for(i=0; i<2; i++){
+   printf("Enter mark %d :",i+1);
+   scanf("%d", &mark);
+   sum = sum + mark;
+  }
+   
+  avg = sum / i - 1;
+   printf("avg id %.2f", avg);
   return 0;
 }
-
--- Tute04.c
+++ Tute04.c
@@ -1,7 +1,5 @@
 /*Exercise 4 - Functions
-
 Implement the three functions minimum(), maximum() and multiply() below the main() function.
-
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
@@ -16,4 +14,16 @@ int main() {
    printf("%d ", maximum(no1, no2));
    printf("%d ", multiply(no1, no2));
    return 0;
+}
+
+int maximum(int num1, int num2){
+    return (num1 > num2 ) ? num1 : num2;
+}
+
+int minimum(int num1, int num2) {
+    return (num1 > num2 ) ? num2 : num1;
+}
+
+int multiply(int num1, int num2) {
+    return  num2 * num1;
 }
